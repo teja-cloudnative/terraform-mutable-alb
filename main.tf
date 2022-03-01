509ece03a212c0ccf58eac46ec2cc5ce1de543f4
tejap <btejapramod@gmail.com>
2022-03-01 11:42:21 +0530

    New Changes

--- main.tf
+++ main.tf
@@ -3,9 +3,21 @@ resource "aws_lb" "frontend" {
   internal                   = false
   load_balancer_type         = "application"
   enable_deletion_protection = false
-  subnets                    = data.terraform_remote_state.vpc.outputs.SUBNETS_ID
+  subnets                    = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNETS_ID
   tags = {
     Name = "frontend-${var.ENV}"
     env  = var.ENV
   }
 }
+
+#resource "aws_lb" "backend" {
+#  name                       = "backend-${var.ENV}"
+#  internal                   = true
+#  load_balancer_type         = "application"
+#  enable_deletion_protection = false
+#  subnets                    = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_ID
+#  tags = {
+#    Name = "backend-${var.ENV}"
+#    env  = var.ENV
+#  }
+#}
