639b5f47b18cc5314e166f9c3887d9961e676317
tejap <btejapramod@gmail.com>
2022-03-04 11:47:05 +0530

    New Changes

---
+++ listeners.tf
@@ -0,0 +1,15 @@
+resource "aws_lb_listener" "backend" {
+  load_balancer_arn = aws_lb.backend.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "OK"
+      status_code  = "200"
+    }
+  }
+}
